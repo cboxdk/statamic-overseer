5ab918ad261bc4f602abcb7d533a19d2f501338e
Jack Sleight <jacksleight@gmail.com>
2024-07-08 17:14:56 +0100

    A fake chart

--- resources/js/components/audits/Listing.vue
+++ resources/js/components/audits/Listing.vue
@@ -5,63 +5,75 @@
             <loading-graphic />
         </div>
 
-        <data-list
-            v-if="!initializing"
-            :visible-columns="columns"
-            :columns="columns"
-            :rows="items"
-            :sort="false"
-            :sort-column="sortColumn"
-            :sort-direction="sortDirection">
-            
-            <div slot-scope="{ filteredRows: rows }">
-                
-                <div class="card p-0 overflow-hidden">
+        <div v-if="!initializing">
 
-                    <data-list-filters
-                        ref="filters"
-                        :filters="filters"
-                        :active-filters="activeFilters"
-                        :active-filter-badges="activeFilterBadges"
-                        :active-count="activeFilterCount"
-                        @changed="filterChanged"
-                    />
-
-                    <div class="overseer-table">
-                        <data-list-table
-                            @sorted="sorted">
-                            <template slot="cell-created_at" slot-scope="{ row: audit }">
-                                <a :href="cp_url(`overseer/executions/${audit.execution_id}`)" class="text-blue">
-                                    {{ $moment(audit.created_at).format('YYYY-MM-DD HH:mm:ss.SSSS') }}
-                                </a>
-                                <div class="text-2xs opacity-75">
-                                    {{ audit.execution_id }}
-                                </div>
-                            </template>
-                            <template slot="cell-initiator" slot-scope="{ row: audit }">
-                                <event-info :event="audit.initiator" />
-                            </template>
-                            <template slot="cell-subject" slot-scope="{ row: audit }">
-                                <subject :audit="audit" />
-                            </template>
-                            <template slot="cell-user" slot-scope="{ row: audit }">
-                                <user :user="audit.user" :impersonator="audit.impersonator" />
-                            </template>
-                        </data-list-table>
-                    </div>
-                </div>
-
-                <data-list-pagination
-                    class="mt-6"
-                    :resource-meta="meta"
-                    :per-page="perPage"
-                    @page-selected="selectPage"
-                    @per-page-changed="changePerPage"
+            <div class="card mb-4">
+                <LineChartGenerator
+                    :chart-options="chart.options"
+                    :chart-data="chart.data"  
+                    :height="300"
                 />
-
             </div>
 
-        </data-list>
+            <data-list
+                
+                :visible-columns="columns"
+                :columns="columns"
+                :rows="items"
+                :sort="false"
+                :sort-column="sortColumn"
+                :sort-direction="sortDirection">
+                
+                <div slot-scope="{ filteredRows: rows }">
+                    
+                    <div class="card p-0 overflow-hidden">
+    
+                        <data-list-filters
+                            ref="filters"
+                            :filters="filters"
+                            :active-filters="activeFilters"
+                            :active-filter-badges="activeFilterBadges"
+                            :active-count="activeFilterCount"
+                            @changed="filterChanged"
+                        />
+    
+                        <div class="overseer-table">
+                            <data-list-table
+                                @sorted="sorted">
+                                <template slot="cell-created_at" slot-scope="{ row: audit }">
+                                    <a :href="cp_url(`overseer/executions/${audit.execution_id}`)" class="text-blue">
+                                        {{ $moment(audit.created_at).format('YYYY-MM-DD HH:mm:ss.SSSS') }}
+                                    </a>
+                                    <div class="text-2xs opacity-75">
+                                        {{ audit.execution_id }}
+                                    </div>
+                                </template>
+                                <template slot="cell-initiator" slot-scope="{ row: audit }">
+                                    <event-info :event="audit.initiator" />
+                                </template>
+                                <template slot="cell-subject" slot-scope="{ row: audit }">
+                                    <subject :audit="audit" />
+                                </template>
+                                <template slot="cell-user" slot-scope="{ row: audit }">
+                                    <user :user="audit.user" :impersonator="audit.impersonator" />
+                                </template>
+                            </data-list-table>
+                        </div>
+                    </div>
+    
+                    <data-list-pagination
+                        class="mt-6"
+                        :resource-meta="meta"
+                        :per-page="perPage"
+                        @page-selected="selectPage"
+                        @per-page-changed="changePerPage"
+                    />
+    
+                </div>
+    
+            </data-list>
+
+        </div>
 
     </div>
 </template>
@@ -70,6 +82,27 @@
 import User from '../common/User.vue';
 import EventInfo from '../events/Info.vue';
 import Subject from './Subject.vue';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+} from 'chart.js'
+import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+)
 
 export default {
 
@@ -79,16 +112,34 @@ export default {
         EventInfo,
         User,
         Subject,
+        LineChartGenerator,
     },
 
     props: ['initialColumns'],
 
     data() {
+        const randomChartData = this.randomChartData();
         return {
             columns: this.initialColumns,
             sortColumn: 'created_at',
             sortDirection: 'desc',
             requestUrl: cp_url('overseer/audits/list'),
+            chart: {
+                data: {
+                    labels: Object.keys(randomChartData).reverse(),
+                    datasets: [
+                        {
+                            label: 'Requests',
+                            backgroundColor: '#43a9ff',
+                            data: Object.values(randomChartData),
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                }
+            },
         }
     },
 
@@ -100,6 +151,19 @@ export default {
                 audit.model_handle,
                 audit.model_id,
             ].filter(item => item !== null).join(' / ');
+        },
+
+        randomChartData() {
+            const today = new Date();
+            const data = {};
+            for (let i = 0; i < 28; i++) {
+                const date = new Date(today);
+                date.setDate(today.getDate() - i);
+                const formattedDate = date.toISOString().split('T')[0];
+                const randomValue = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
+                data[formattedDate] = randomValue;
+            }
+            return data;
         },
 
     }
