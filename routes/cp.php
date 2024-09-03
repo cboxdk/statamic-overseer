74118a91e9189b3d60eb49763333b2710a70dadd
Jack Sleight <jacksleight@gmail.com>
2024-09-03 12:57:03 +0100

    Add event analytics and real chart

--- routes/cp.php
+++ routes/cp.php
@@ -1,6 +1,7 @@
 <?php
 
 use Cboxdk\StatamicOverseer\Http\Controllers\CP\AuditsController;
+use Cboxdk\StatamicOverseer\Http\Controllers\CP\EventAnalyticsController;
 use Cboxdk\StatamicOverseer\Http\Controllers\CP\EventsController;
 use Cboxdk\StatamicOverseer\Http\Controllers\CP\ExecutionsController;
 
@@ -11,6 +12,7 @@ Route::middleware('statamic.cp.authenticated')
         Route::get('executions/list', [ExecutionsController::class, 'list']);
         Route::resource('executions', ExecutionsController::class)->only(['index', 'show']);
         Route::get('events/list', [EventsController::class, 'list']);
+        Route::get('events/analytics', [EventAnalyticsController::class, 'index']);
         Route::resource('events', EventsController::class)->only(['index']);
         Route::get('audits/list', [AuditsController::class, 'list']);
         Route::resource('audits', AuditsController::class)->only(['index']);
